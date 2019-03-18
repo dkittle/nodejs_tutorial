4f6c9dfa4a9d447c229eb32e017d8297b2ca8002
dkittle <don.kittle@weightwatchers.com>
2019-03-18 11:52:37 -0400

    Working through a tutorial on promises

---
+++ promise_tutorial.js
@@ -0,0 +1,45 @@
+
+let promiseToCleanRoom = new Promise(function(resolve, reject){
+   let isClean = false;
+
+   if (isClean) {
+       resolve('clean');
+   } else {
+       reject("not clean");
+   }
+});
+
+promiseToCleanRoom.then(function(fromResolve) {
+    console.log('The room is ' + fromResolve);
+}).catch(function(fromReject) {
+    console.log('the room is ' + fromReject);
+});
+
+
+// Chaining
+let cleanTheRoom = function() {
+    return new Promise(function(resolve, reject) {
+        resolve('cleaned my room');
+    });
+};
+
+let takeOutGarbage = function(message) {
+    return new Promise(function(resolve, reject) {
+       resolve(message + ', garbage was taken out');
+    });
+};
+
+let eatIcecream = function(message) {
+    return new Promise(function(resolve, reject) {
+        resolve(message + ', and ate icecream');
+    });
+};
+
+cleanTheRoom().then(function(result) {
+    return takeOutGarbage(result);
+}).then(function(result) {
+    return eatIcecream(result);
+}).then(function(result) {
+    console.log('finished work: ' + result);
+});
+
