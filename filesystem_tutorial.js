17c1529e2df7d537a89e1cf54dbb46394bc76bc2
dkittle <don.kittle@weightwatchers.com>
2019-03-20 15:02:11 -0400

    gotta say, I really like those double quotes

--- filesystem_tutorial.js
+++ filesystem_tutorial.js
@@ -1,27 +1,27 @@
-const fs = require('fs');
+const fs = require("fs");
 
 // Playing with files
-fs.writeFile('example.txt', 'This is an example', (err) =>{
+fs.writeFile("example.txt", "This is an example", (err) =>{
    if (err)
        console.log(err);
     else {
-       console.log('file successfully created');
-       fs.readFile('example.txt', 'utf8', (err, file)=>{
+       console.log("file successfully created");
+       fs.readFile("example.txt", "utf8", (err, file)=>{
           if (err)
               console.log(err);
           else {
               console.log("------------ file contents ------------")
               console.log(file);
-              fs.rename('example.txt', 'example2.txt', (err)=>{
+              fs.rename("example.txt", "example2.txt", (err)=>{
                   if (err)
                       console.log(err);
                   else {
-                      console.log('renamed the file');
-                      fs.unlink('example2.txt', (err)=>{
+                      console.log("renamed the file");
+                      fs.unlink("example2.txt", (err)=>{
                           if (err)
                               console.log(err);
                           else
-                              console.log('everthing has been cleaned up');
+                              console.log("everthing has been cleaned up");
                       });
                   }
               });
